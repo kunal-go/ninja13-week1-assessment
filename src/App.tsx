515b82e47ad7cf1e1b3df0d3db762c027ef8b8f3
Kunal Gosrani <iamkunalgosrani@gmail.com>
2021-12-23 00:58:28 +0530

    feat: fetch data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
+import { useCallback, useEffect, useState } from "react"
+import { IMovie } from "./IMovie"
+import MovieCard from "./MovieCard"
+
 export default function App() {
-	return <div className="App">Movie App</div>
+	const apiKey = process.env.REACT_APP_API_KEY
+	const pageNo = 1 // temporary hard-coded value
+
+	const [movies, setMovies] = useState<IMovie[]>([])
+
+	const fetchMovies = useCallback(async () => {
+		const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&page=${pageNo}`
+
+		const response = await fetch(url)
+		const data = await response.json()
+		setMovies(data.results)
+	}, [])
+
+	useEffect(() => {
+		fetchMovies()
+	}, [])
+
+	return (
+		<div className="App">
+			Movie App
+			<div>
+				{movies.map((movie) => (
+					<MovieCard movie={movie} />
+				))}
+			</div>
+		</div>
+	)
 }
