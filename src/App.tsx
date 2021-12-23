11c7de747230de489a5154f9b5185ee798e176d0
Kunal Gosrani <iamkunalgosrani@gmail.com>
2021-12-23 23:58:09 +0530

    show error message
    - show error message if api fails to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,20 @@ export default function App() {
 	const pageNo = 1 // temporary hard-coded value
 
 	const [movies, setMovies] = useState<IMovie[]>([])
+	const [errorMessage, setErrorMessage] = useState<string>()
 
 	const fetchMovies = useCallback(async () => {
 		const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&page=${pageNo}`
 
-		const response = await fetch(url)
-		const data = await response.json()
-		setMovies(data.results)
+		try {
+			const response = await fetch(url)
+			const data = await response.json()
+			setMovies(data.results)
+		} catch (err) {
+			if (err instanceof Error) {
+				setErrorMessage(err.message)
+			}
+		}
 	}, [apiKey, pageNo])
 
 	useEffect(() => {
@@ -24,9 +31,11 @@ export default function App() {
 	return (
 		<div className="app">
 			<h1>Popular Movies</h1>
+
+			{errorMessage && <div className="error">{errorMessage}</div>}
 			<div className="movie-card-list-container">
 				{movies.map((movie) => (
-					<MovieCard movie={movie} className="movie-card" />
+					<MovieCard key={movie.id} movie={movie} className="movie-card" />
 				))}
 			</div>
 		</div>
