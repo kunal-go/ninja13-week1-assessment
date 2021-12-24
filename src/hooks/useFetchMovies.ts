3352e27a1973acc9c18fc7694d9ad1ea896691cf
Kunal Gosrani <iamkunalgosrani@gmail.com>
2021-12-24 15:48:29 +0530

    feat: custom hook to fetch movies

---
+++ src/hooks/useFetchMovies.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+import { IMovie } from "../components/MovieCard/IMovie"
+
+export function useFetchMovies(pageNumber: number) {
+	const apiKey = process.env.REACT_APP_API_KEY
+	const [movies, setMovies] = useState<IMovie[]>([])
+	const [isLoading, setIsLoading] = useState(false)
+	const [errorMessage, setErrorMessage] = useState<string>()
+	const [hasMore, setHasMore] = useState(false)
+
+	useEffect(() => {
+		setIsLoading(true)
+
+		const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&page=${pageNumber}`
+
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => {
+				setMovies((currentMovies) => [...currentMovies, ...data.results])
+				setHasMore(data.results.length > 0)
+			})
+			.catch((err) => {
+				if (err instanceof Error) {
+					setErrorMessage(err.message)
+				}
+				setErrorMessage("Something went wrong")
+			})
+			.finally(() => {
+				setIsLoading(false)
+			})
+	}, [apiKey, pageNumber])
+
+	return { movies, isLoading, hasMore, errorMessage }
+}
